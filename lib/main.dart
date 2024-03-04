7b93f9e896511cc6cb772298276f3ec3fe9a6671
hesham <heshamhamdan51@gmail.com>
2024-03-04 16:22:56 +0200

    create a register adaptor of note model

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:note_app/Consts.dart';
+import 'package:note_app/Models/note_model.dart';
 import 'package:note_app/Views/notes_view.dart';
 
 void main() async {
   await Hive.initFlutter();
 
   Hive.openBox(knotesbox);
+  Hive.registerAdapter(NoteModelsAdapter());
   runApp(const NoteApp());
 }
 
